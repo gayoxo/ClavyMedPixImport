59a6ebff3309678e325e93f3a0fec15c75e52bc4
Joaquin Gayoso-Cabada <jgayoso@ucm.es>
2018-06-20 15:07:41 +0200

    Estadistico

---
+++ ClavyMedPixImport/src/fdi/ucm/server/importparser/medpixcounter/LoadCollectionMedPixCounterJSon.java
@@ -0,0 +1,289 @@
+/**
+ * 
+ */
+package fdi.ucm.server.importparser.medpixcounter;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import fdi.ucm.server.modelComplete.collection.CompleteCollection;
+import fdi.ucm.server.modelComplete.collection.CompleteCollectionAndLog;
+import fdi.ucm.server.modelComplete.collection.document.CompleteDocuments;
+
+/**
+ * @author Joaquin Gayoso Cabada
+ *
+ */
+public class LoadCollectionMedPixCounterJSon{
+
+	
+//	private List<CompleteElementTypetopicIDTC> ListTopicID;
+//	private List<CompleteElementTypeencounterIDImage> ListImageEncounterTopics;
+//	private List<CompleteElementTypeencounterIDImage> ListImageEncounter;
+	private CompleteCollection CC;
+	private ArrayList<String> Logs;
+//	private CompleteLinkElementType encounterIDL;
+	private HashSet<String> encounterID;
+//	private HashMap<String,List<CompleteDocuments>> topicID;
+//	private CompleteLinkElementType encounterIDLC;
+//	private CompleteLinkElementType topicIDTC;
+	public static boolean consoleDebug=false;
+//	private CompleteLinkElementType topicIDIDLC;
+	
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		LoadCollectionMedPixCounterJSon LC=new LoadCollectionMedPixCounterJSon();
+		LoadCollectionMedPixCounterJSon.consoleDebug=true;
+		
+		
+		
+		CompleteCollectionAndLog Salida=LC.processCollecccion(new ArrayList<String>());
+		if (Salida!=null)
+			{
+			
+			System.out.println("Correcto");
+			
+			for (String warning : Salida.getLogLines())
+				System.err.println(warning);
+
+			
+			System.exit(0);
+			
+			}
+		else
+			{
+			System.err.println("Error");
+			System.exit(-1);
+			}
+	}
+
+	
+
+	public CompleteCollectionAndLog processCollecccion(ArrayList<String> dateEntrada) {
+		try {
+			CompleteCollectionAndLog Salida=new CompleteCollectionAndLog();
+			CC=new CompleteCollection("MedPix", new Date()+"");
+			Salida.setCollection(CC);
+			Logs=new ArrayList<String>();
+			Salida.setLogLines(Logs);
+			encounterID=new  HashSet<String>();
+//			topicID=new HashMap<String,List<CompleteDocuments>>();
+//			ListImageEncounter=new ArrayList<CompleteElementTypeencounterIDImage>();
+//			ListImageEncounterTopics=new ArrayList<CompleteElementTypeencounterIDImage>();
+//			ListTopicID=new ArrayList<CompleteElementTypetopicIDTC>();
+			
+			ProcesaCasos();
+			ProcesaCasoID();
+			
+			return Salida;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+
+
+
+
+
+
+
+	private void ProcesaCasoID() {
+		
+		ProcesaValoresCasoIDJson();
+	}
+
+
+
+	
+private void ProcesaValoresCasoIDJson() {
+	
+	Long counter=0L;
+	JSONArray Salida = new JSONArray();
+	HashSet<String> tabla = ProcesaGramaticaCasoID();
+	
+		int ite=1;
+		for (String Entryvalues : encounterID) {
+		
+			
+			System.out.println(ite++ + "/"+encounterID.size());
+			
+			String IDvalues=Entryvalues;
+			
+			JSONObject jod=new JSONObject();
+			
+			try {
+			URL F=new URL("https://medpix.nlm.nih.gov/rest/encounter.json?encounterID="+IDvalues);
+			
+			InputStream is = F.openStream();
+    	    
+    	      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+    	      String jsonText = readAll(rd);
+    	      JSONObject json = new JSONObject(jsonText);
+
+    	      CompleteDocuments cd=new CompleteDocuments(CC, "", "");
+				CC.getEstructuras().add(cd);
+    	      
+				
+				for (String entryTabla : tabla) {
+					String Valor = json.get(entryTabla).toString();
+					
+					jod.put(entryTabla,Valor);
+					
+					counter=0L+Valor.split("\\s+|\n").length;			
+				}
+				
+				
+	      Salida.put(jod);
+	       	  
+			} catch (Exception e) {
+				e.printStackTrace();
+				Logs.add("Error con la carga de documento->encounterID: "+IDvalues);
+//				throw new RuntimeException("No tiene editor o los elementos son incorrectos");
+			}
+			
+			
+			
+			
+		}
+		
+		System.out.println("J->"+Salida.toString());
+		
+		System.out.println("D->"+encounterID.size());
+		
+		System.out.println("M->"+Math.round(counter/encounterID.size()));
+		
+		try {
+			PrintWriter writer = new PrintWriter("Out.txt", "UTF-8");
+			writer.println(Salida.toString());
+			writer.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+
+
+
+
+
+	private void ProcesaCasos() {
+
+
+		ProcesaValores();
+		
+	}
+
+
+
+	private void ProcesaValores() {
+		
+		char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+		
+		for (char c : alphabet) {
+			 try {
+		        	URL F=new URL("https://medpix.nlm.nih.gov/rest/encounter/diagnosis.json?diagnosis="+c);
+		        	
+		        	System.out.println(F.toString());
+		        	
+		        	 InputStream is = F.openStream();
+		        	    try {
+		        	      BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+		        	      String jsonText = readAll(rd);
+//		        	      System.out.println(jsonText);
+		        	      JSONArray json = new JSONArray(jsonText);
+//		        	      System.out.println(json.toString());
+//		        	      System.out.println(json.get("encounterID"));
+		        	      for (int i = 0; i < json.length(); i++) {
+		        	    	  JSONObject JO=json.getJSONObject(i);
+		        	    	  if (JO.get("encounterID")!=null)
+		        	    		  encounterID.add(JO.get("encounterID").toString());
+						}
+		        	      
+		        	    } finally {
+		        	      is.close();
+		        	    }
+		       	  
+				} catch (Exception e) {
+					if (consoleDebug)
+					e.printStackTrace();
+					Logs.add("Error con la carga de listas de documento");
+//					throw new RuntimeException("No tiene editor o los elementos son incorrectos");
+				}
+		}
+		
+       
+        
+	}
+
+	 private static String readAll(Reader rd) throws IOException {
+		    StringBuilder sb = new StringBuilder();
+		    int cp;
+		    while ((cp = rd.read()) != -1) {
+		      sb.append((char) cp);
+		    }
+		    return sb.toString();
+		  }
+
+
+
+	
+	
+	
+	private HashSet<String> ProcesaGramaticaCasoID() {
+		HashSet<String> Salida=new HashSet<String>();
+		
+	
+		Salida.add("encounterID");	
+		Salida.add("dxHow");
+		Salida.add("history");
+//		Salida.add("age");
+//		Salida.add("sex");
+//		Salida.add("race");
+		Salida.add("diagnosis");
+		Salida.add("exam");
+//		Salida.add("authorID");
+//		Salida.add("authorName");
+//		Salida.add("authorAffiliation");
+//		Salida.add("authorImage");
+//		Salida.add("authorEmail");
+//		Salida.add("approverID");
+//		Salida.add("approverEmail");
+//		Salida.add("approverName");
+//		Salida.add("approverAffiliation");
+//		Salida.add("approverImage");
+		Salida.add("findings");
+		Salida.add("ddx");
+		Salida.add("txFollowup");
+		Salida.add("discussion");
+//		Salida.add("topicID");
+		Salida.add("mCaseID");
+//		Salida.add("error");
+//		Salida.add("contributorsCSV");
+//		Salida.add("affiliation");
+//		Salida.add("affiliationID");
+//		Salida.add("affiliationLogo");
+//		Salida.add("mediaList");
+		
+		return Salida;
+	}
+
+
+
+}
